668e716a2433083617bfc0a1742315aa13e91567
thangajananikannan <thangajananikannan@gmail.com>
2026-02-18 18:14:33 +0530

    add

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://lernevo-backend-staging-771297649928.us-central1.run.app/api',
+  baseURL: 'https://lernevo-backend-prod-1025793460743.asia-southeast1.run.app',
   headers: {
     'Content-Type': 'application/json',
   },
