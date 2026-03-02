841c71d799dc77c655cf0bf514230cf8e27f7030
Saranya <saranyapandiyarajan30@gmail.com>
2026-03-02 20:13:14 +0530

    add

--- frontend/src/start.sh
+++ frontend/src/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-# Replace ${PORT} in nginx config template with actual PORT environment variable
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-# Start nginx
+
+echo "Cloud Run PORT value: $PORT"
+
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "Generated nginx config:"
+cat /etc/nginx/conf.d/default.conf
+
 nginx -g 'daemon off;'
