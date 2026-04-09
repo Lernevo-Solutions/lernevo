1826bf12a7592164b3bafa673cf2708669298ffd
Saranya <saranyapandiyarajan30@gmail.com>
2026-04-09 12:02:39 +0530

    add

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://lernevo-backend-staging-771297649928.us-central1.run.app/api',
+  baseURL: 'https://lernevo-frontend-771297649928.us-central1.run.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
